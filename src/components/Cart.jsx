c5b097890b278ded9c82718f52173b966f649dca
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-06-28 18:48:49 +0200

    276. Adding a Custom Input Component & Managing Modal Visibility

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -13,11 +13,18 @@ export default function Cart() {
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
         return totalPrice + item.quantity * item.price
     }, 0)
+
     function handleCloseCart() {
         userProgressCtx.hideCart()
     }
+    function handleGoToCheckout() {
+        userProgressCtx.showCheckout()
+    }
+
     return (
-        <Modal className='cart' open={userProgressCtx.progress==='cart'}>
+        <Modal className='cart'
+               open={userProgressCtx.progress === 'cart'}
+               onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null }>
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map(item => (
@@ -34,7 +41,9 @@ export default function Cart() {
             <p className='cart-total'>{currencyFormatter.format(cartTotal)}</p>
             <p className='modal-actions'>
                 <Button textonly onClick={handleCloseCart}>Close</Button>
-                <Button onClick={handleCloseCart}>Go to Checkout</Button>
+                {
+                    cartCtx.items.length > 0 && <Button onClick={handleGoToCheckout}>Go to Checkout</Button>
+                }
             </p>
         </Modal>
     )
---
+++ src/components/CheckOut.jsx
@@ -0,0 +1,40 @@
+import Modal from "./UI/Modal.jsx";
+import {useContext} from "react";
+import CartContext from "../store/CartContext.jsx";
+import {currencyFormatter} from "../utils/formatting.js";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
+
+export default function CheckOut() {
+    const cartCtx = useContext(CartContext)
+    const userProgressCtx = useContext(UserProgressContext)
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
+        return totalPrice + item.quantity * item.price
+    }, 0)
+    function handleClose() {
+        userProgressCtx.hideCheckout();
+    }
+    function handleGoToCheckout() {
+        userProgressCtx.showCheckout()
+    }
+    return (
+        <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+            <form>
+                <h2>Checkout</h2>
+                <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+                <Input label="Full Name" type="text" id="full-name"/>
+                <Input label="E-mail Address" type="email" id="email"/>
+                <Input label="Street" type="text" id="street"/>
+                <div>
+                    <Input label="Postal Code" type="text" id="postal-code"/>
+                    <Input label="City" type="text" id="city"/>
+                </div>
+                <p className="modal-actions">
+                    <Button type="button" textonly onClick={handleClose}>Close</Button>
+                    <Button onClick={handleGoToCheckout}>Submit Order</Button>
+                </p>
+            </form>
+        </Modal>
+    )
+}
