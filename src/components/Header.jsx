3c8ee6b93b0a7a45590e354169f3bbdbdb2def1f
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-06-28 10:29:13 +0200

    271,272. Using the Cart Context & Reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import logoImg from '../assets/logo.jpg'
 import Button from "./UI/Button.jsx";
+import {useContext} from "react";
+import CartContext from "../store/CartContext.jsx";
 export default function Header() {
+    const cartCtx = useContext(CartContext)
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item)=>{
+        return totalNumberOfItems+item.quantity
+    }, 0)
     return (
         <header id="main-header">
             <div id="title">
@@ -8,7 +14,7 @@ export default function Header() {
                 <h1>React Food</h1>
             </div>
             <nav>
-                <Button textonly>Cart (0)</Button>
+                <Button textonly>Cart ({totalCartItems})</Button>
             </nav>
         </header>
     )
